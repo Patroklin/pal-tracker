58e8c047ec52c381945080f6ab7fb35525ffde9e
Patrick <patroklin@gmail.com>
2020-09-21 14:17:14 +0000

    Including env. variables.

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,37 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    private final String port, memory, instanceIndex, instanceAddress;
+
+    public EnvController(
+            @Value("port:NOT SET") String port,
+            @Value("memory.limit:NOT SET") String memory,
+            @Value("cf.instance.index:NOT SET") String instanceIndex,
+            @Value("cf.instance.addr:NOT SET") String instanceAddress) {
+        this.port = port;
+        this.memory = memory;
+        this.instanceIndex = instanceIndex;
+        this.instanceAddress = instanceAddress;
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        Map<String, String> env = new HashMap<>();
+
+        env.put("PORT", port);
+        env.put("MEMORY_LIMIT", memory);
+        env.put("CF_INSTANCE_INDEX", instanceIndex);
+        env.put("CF_INSTANCE_ADDR", instanceAddress);
+
+        return env;
+    }
+}
