745cb1c865119db25064834fc4cf820dda27e889
Patrick <patroklin@gmail.com>
2020-09-21 10:24:36 +0000

    Changing the message as I was trying to be smart guy :)

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -8,6 +8,6 @@ public class WelcomeController {
 
     @GetMapping("/")
     public String sayHello() {
-        return "Hello my friend!! Welcome to the dark side";
+        return "hello";
     }
 }
